789983d8f9767bea4f78ce4aff35b3bbc2acafc2
zhuxianyong <zhuxianyong@eversec.cn>
2018-09-06 13:34:37 +0800

    新增404页面

---
+++ src/pages/404/index.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="page-404 abs flex-center">
+  <div class="content">
+    <h1>404</h1>
+    <p>找不到此页面：<span>{{$route.path}}</span></p>
+    <p>
+      <router-link class="c-8" to="/">回到首页</router-link>
+    </p>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'page-404'
+}
+</script>
+
+<style lang="less">
+.page-404{
+  .content{width: 500px;height: 300px;}
+  h1{font-size: 60px;}
+}
+</style>

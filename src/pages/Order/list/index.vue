6d839489ae514d405538fa5980dea44170d3930c
hedong <hedong@ibeesaas.com>
2019-01-21 23:21:33 +0800

    增加零件查询条件和订单查询条件

--- src/pages/Order/list/index.vue
+++ src/pages/Order/list/index.vue
@@ -1,111 +1,122 @@
 <template>
-<div class="page-part-list">
-  <div class="flex-v-center tool-bar">
-    <!-- <div class="flex-v-center search-bar" style="margin-right: 20px;">
-      <i class="icon f-20 c-8">search</i>
-      <select v-model="filter.type" class="f-14" @change="$refs.list.update(true)">
-        <option value="">所有库房</option>
-        <option value="1">原料库</option>
-        <option value="2">成品库</option>
-      </select>
-      <i class="icon" style="margin-left: -20px;pointer-events:none;">arrow_drop_down</i>
-    </div> -->
-    <div class="flex-item"></div>
-    <router-link to="/order/add">
-      <btn class="b" flat color="#008eff">新增订单</btn>
-    </router-link>
-  </div>
-  <div class="flex-item scroll-y">
-    <data-list ref="list" :param="filter" url="/haolifa/order-product/pageInfo" method="post">
-      <tr slot="header">
-        <th style="width: 60px;">序号</th>
-        <th>订单编号</th>
-        <th>成品合同订单号</th>
-        <th style="width:50px;">订单合同URL</th>
-        <th>审批人</th>
-        <th>审批结果</th>
-        <th>创建时间</th>
-        <th>更新时间</th>
-        <th class="t-right" style="width: 80px;">操作</th>
-      </tr>
-      <!-- item: 当前行数据; index: 当前行数 -->
-      <template slot="item" slot-scope="{ item, index }">
-        <td class="c-a">{{index}}</td>
-        <td>{{item.orderNo}}</td>
-        <td>{{item.contractOrderNo}}</td>
-        <td><a class='fixed-length' :href="item.orderContractUrl" :title="item.orderContractUrl">{{item.orderContractUrl}}</a></td>
-        <td>{{item.auditUserId}}</td>
-        <td>{{item.auditResult}}</td>
-        <td>{{item.auditInfo}}</td>
-        <td>{{item.auditTime}}</td>
-        <td>{{item.createTime}}</td>
-        <td>{{item.updateTime}}</td>
-        <td class="t-right">
-          <a href="javascript:;" class="blue" @click="progress(item)"  style="margin-right: 3px;">发起流程</a> |
-          <a href="javascript:;" class="red" @click="remove(item)" v-if="item.orderStatus==0" style="margin-right: 3px;">删除</a>
-          <!-- <icon-btn small @click="remove(item)">delete</icon-btn> -->
-        </td>
-      </template>
-    </data-list>
-  </div>
-</div>
+    <div class="page-orders-list">
+        <div class="flex-v-center tool-bar">
+            <div class="flex-v-center search-bar" style="margin-right: 20px;">
+                <i class="icon f-20 c-8">search</i>
+                <input type="text" class="flex-item" v-model="filter.orderNo" @change="$refs.list.update(true)"
+                       placeholder="订单号" style="width: 200px;">
+                <select v-model="filter.orderStatus" class="f-14" @change="$refs.list.update(true)">
+                    <option value="-1">全部</option>
+                    <option v-for="item in orderStatusList" :value="item.value" v-bind:key="item.value">{{item.text}}
+                    </option>
+                </select>
+                <i class="icon" style="margin-left: -20px;pointer-events:none;">arrow_drop_down</i>
+            </div>
+            <div class="flex-item"></div>
+            <router-link to="/order/add">
+                <btn class="b" flat color="#008eff">新增订单</btn>
+            </router-link>
+        </div>
+        <div class="flex-item scroll-y">
+            <data-list ref="list" :param="filter" url="/haolifa/order-product/pageInfo" method="post">
+                <tr slot="header">
+                    <th style="width: 60px;">序号</th>
+                    <th>订单编号</th>
+                    <th>成品合同订单号</th>
+                    <th style="width:50px;">订单合同URL</th>
+                    <th>订单状态</th>
+                    <th>创建时间</th>
+                    <th class="t-right" style="width: 80px;">操作</th>
+                </tr>
+                <!-- item: 当前行数据; index: 当前行数 -->
+                <template slot="item" slot-scope="{ item, index }">
+                    <td class="c-a">{{index}}</td>
+                    <td>{{item.orderNo}}</td>
+                    <td>{{item.orderContractNo}}</td>
+                    <td><a class='fixed-length' :href="item.orderContractUrl" :title="item.orderContractUrl">{{item.orderContractUrl}}</a>
+                    </td>
+                    <td>{{orderStatusList[item.orderStatus].text}}</td>
+                    <td>{{item.createTime}}</td>
+                    <td class="t-right">
+                        <a href="javascript:;" class="blue" @click="progress(item)" v-if="item.orderStatus==0" style="margin-right: 3px;">发起流程|</a>
+
+                        <a href="javascript:;" class="red" @click="remove(item)" v-if="item.orderStatus==0||item.orderStatus==14"
+                           style="margin-right: 3px;">删除</a>
+                        <!-- <icon-btn small @click="remove(item)">delete</icon-btn> -->
+                    </td>
+                </template>
+            </data-list>
+        </div>
+    </div>
 </template>
 
 <script>
-import DataList from '@/components/datalist'
-export default {
-  name: 'page-orders-list',
-  components: { DataList },
-  data () {
-    return {
-      loading: false,
-      filter: {
-        // type: ''
-      }
-    }
-  },
-  methods: {
-    edit (item) {
-      this.$router.push(`/dilivery-bills/edit?id=${item.id}`)
-    },
-    progress (item) {
-      this.$http.post('/haolifa/flowInstance/create', {
-        flowId: 1, formId: item.id, formType: 1, formNo: item.orderNo, summary: '生产订单审批' }).then(
-        res => {
-          this.loading = false
-          this.$toast(`发起流程成功,流程ID: ${res.instanceId}`)
+    import DataList from '@/components/datalist'
+
+    export default {
+        name: 'page-orders-list',
+        components: {DataList},
+        data() {
+            return {
+                loading: false,
+                orderStatusList:[],
+                filter: {
+                    orderNo: '',
+                    orderStatus: -1
+                }
+            }
+        },
+        created() {
+            this.getOrderStatusList()
+        },
+        methods: {
+            getOrderStatusList() {
+                this.$http.get('/haolifa/order-product/order-status-list').then(res => {
+                    this.orderStatusList = res.map(item => {
+                        return {value: item.code, text: item.desc}
+                    });
+                })
+            },
+            progress(item) {
+                this.$http.post('/haolifa/flowInstance/create', {
+                    flowId: 1, formId: item.id, formType: 1, formNo: item.orderNo, summary: '生产订单审批'
+                }).then(
+                    res => {
+                        this.loading = false
+                        this.$toast(`发起流程成功,流程ID: ${res.instanceId}`)
+                    }
+                )
+            },
+            remove(item) {
+                this.$confirm({
+                    title: '删除确认',
+                    text: `您确定要删除以下发货通知单吗？<br>${item.deliveryNo}`,
+                    color: 'red',
+                    btns: ['取消', '删除'],
+                    yes: () => {
+                        this.$http.delete(`/haolifa/order-product/delete/${item.id}`).then(res => {
+                            this.$toast('删除成功')
+                            this.$refs.list.update()
+                        }).catch(e => {
+                            this.$toast(e.msg)
+                        })
+                    }
+                })
+            }
         }
-      )
-    },
-    remove (item) {
-      this.$confirm({
-        title: '删除确认',
-        text: `您确定要删除以下发货通知单吗？<br>${item.deliveryNo}`,
-        color: 'red',
-        btns: ['取消', '删除'],
-        yes: () => {
-          this.$http.delete(`/haolifa/order-product/delete/${item.id}`).then(res => {
-            this.$toast('删除成功')
-            this.$refs.list.update()
-          }).catch(e => {
-            this.$toast(e.msg)
-          })
-        }
-      })
     }
-  }
-}
 </script>
 
 <style lang="less">
-.page-part-list{
-  //
-}
-.fixed-length{
-  width: 100px;
-  display: block;
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-}
+    .page-part-list {
+        //
+    }
+
+    .fixed-length {
+        width: 100px;
+        display: block;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+    }
 </style>
--- src/pages/Supporting/part/index.vue
+++ src/pages/Supporting/part/index.vue
@@ -1,91 +1,109 @@
 <template>
-<div class="page-part-list">
-  <div class="flex-v-center tool-bar">
-    <!-- <div class="flex-v-center search-bar" style="margin-right: 20px;">
-      <i class="icon f-20 c-8">search</i>
-      <select v-model="filter.type" class="f-14" @change="$refs.list.update(true)">
-        <option value="">所有库房</option>
-        <option value="1">原料库</option>
-        <option value="2">成品库</option>
-      </select>
-      <i class="icon" style="margin-left: -20px;pointer-events:none;">arrow_drop_down</i>
-    </div> -->
-    <div class="flex-item"></div>
-    <router-link to="/part/add">
-      <btn class="b" flat color="#008eff">新增零件</btn>
-    </router-link>
-  </div>
-  <div class="flex-item scroll-y">
-    <data-list ref="list" page-num-str="currentPage" :param="filter" url="/haolifa/material/pageInfo" method="get">
-      <tr slot="header">
-        <th style="width: 60px;">序号</th>
-        <th>零件名称</th>
-        <th>零件类型</th>
-        <th>库存</th>
-        <th>单位</th>
-        <th>图号</th>
-        <th>配套数量</th>
-        <th>可替换零件</th>
-        <th class="t-right" style="width: 80px;">操作</th>
-      </tr>
-      <!-- item: 当前行数据; index: 当前行数 -->
-      <template slot="item" slot-scope="{ item, index }">
-        <td class="c-a">{{index}}</td>
-        <td>{{item.name}}</td>
-        <td>{{item.materialClassifyName}}</td>
-        <td>{{item.currentQuantity}}</td>
-        <td>{{item.unit}}</td>
-        <td>{{item.graphNo}}</td>
-        <td>{{item.supportQuantity}}</td>
-        <td>{{item.replaceGraphNos}}</td>
-        <td class="t-right">
-          <a href="javascript:;" class="blue" @click="edit(item)" style="margin-right: 3px;">编辑</a> |
-          <a href="javascript:;" class="red" @click="remove(item)" style="margin-right: 3px;">删除</a>
-        </td>
-      </template>
-    </data-list>
-  </div>
-</div>
+    <div class="page-part-list">
+        <div class="flex-v-center tool-bar">
+            <div class="flex-v-center search-bar" style="margin-right: 20px;">
+                <i class="icon f-20 c-8">search</i>
+                <input type="text" class="flex-item" v-model="filter.graphNo" @change="$refs.list.update(true)" placeholder="图号" style="width: 200px;">
+                <input type="text" class="flex-item" v-model="filter.specifications" @change="$refs.list.update(true)" placeholder="型号" style="width: 100px;">
+                <input type="text" class="flex-item" v-model="filter.model" @change="$refs.list.update(true)" placeholder="规格" style="width: 100px;">
+                <select v-model="filter.classifyId" class="f-14" @change="$refs.list.update(true)">
+                    <option value="0">全部</option>
+                    <option v-for="item in classifyList" :value="item.value" v-bind:key="item.value">{{item.text}}
+                    </option>
+                </select>
+                <i class="icon" style="margin-left: -20px;pointer-events:none;">arrow_drop_down</i>
+            </div>
+            <div class="flex-item"></div>
+            <router-link to="/part/add">
+                <btn class="b" flat color="#008eff">新增零件</btn>
+            </router-link>
+        </div>
+        <div class="flex-item scroll-y">
+            <data-list ref="list" page-num-str="pageNum" :param="filter" url="/haolifa/material/pageInfo" method="post">
+                <tr slot="header">
+                    <th style="width: 60px;">序号</th>
+                    <th>零件名称</th>
+                    <th>零件类型</th>
+                    <th>图号</th>
+                    <th>型号</th>
+                    <th>规格</th>
+                    <th>配套数量</th>
+                    <th>库存</th>
+                    <th class="t-right" style="width: 80px;">操作</th>
+                </tr>
+                <!-- item: 当前行数据; index: 当前行数 -->
+                <template slot="item" slot-scope="{ item, index }">
+                    <td class="c-a">{{index}}</td>
+                    <td>{{item.name}}</td>
+                    <td>{{item.materialClassifyName}}</td>
+                    <td>{{item.graphNo}}</td>
+                    <td>{{item.model}}</td>
+                    <td>{{item.specifications}}</td>
+                    <td>{{item.supportQuantity}}</td>
+                    <td>{{item.currentQuantity}}</td>
+                    <td class="t-right">
+                        <a href="javascript:;" class="blue" @click="edit(item)" style="margin-right: 3px;">编辑</a> |
+                        <a href="javascript:;" class="red" @click="remove(item)" style="margin-right: 3px;">删除</a>
+                    </td>
+                </template>
+            </data-list>
+        </div>
+    </div>
 </template>
 
 <script>
-import DataList from '@/components/datalist'
-export default {
-  name: 'page-part-list',
-  components: { DataList },
-  data () {
-    return {
-      filter: {
-        type: ''
-      }
-    }
-  },
-  methods: {
-    edit (item) {
-      this.$router.push(`/part/edit?id=${item.id}`)
-    },
-    remove (item) {
-      this.$confirm({
-        title: '删除确认',
-        text: `您确定要删除以下库房吗？<br><b>${item.name}</b>`,
-        color: 'red',
-        btns: ['取消', '删除'],
-        yes: () => {
-          this.$http.delete(`/haolifa/material/delete/${item.id}`).then(res => {
-            this.$toast('删除成功')
-            this.$refs.list.update()
-          }).catch(e => {
-            this.$toast(e.msg)
-          })
+    import DataList from '@/components/datalist'
+
+    export default {
+        name: 'page-part-list',
+        components: {DataList},
+        data() {
+            return {
+                classifyList: [],
+                filter: {
+                    graphNo: '',
+                    model: '',
+                    specifications: '',
+                    classifyId: 0
+                }
+            }
+        },
+        created() {
+            this.getClassifyList()
+        },
+        methods: {
+            edit(item) {
+                this.$router.push(`/part/edit?id=${item.id}`)
+            },
+            remove(item) {
+                this.$confirm({
+                    title: '删除确认',
+                    text: `您确定要删除以下库房吗？<br><b>${item.name}</b>`,
+                    color: 'red',
+                    btns: ['取消', '删除'],
+                    yes: () => {
+                        this.$http.delete(`/haolifa/material/delete/${item.id}`).then(res => {
+                            this.$toast('删除成功')
+                            this.$refs.list.update()
+                        }).catch(e => {
+                            this.$toast(e.msg)
+                        })
+                    }
+                })
+            },
+            getClassifyList() {
+                this.$http.get('/haolifa/material/classify/list').then(res => {
+                    this.classifyList = res.map(item => {
+                        return {value: item.id, text: item.classifyName}
+                    });
+                })
+            }
         }
-      })
     }
-  }
-}
 </script>
 
 <style lang="less">
-.page-part-list{
-  //
-}
+    .page-part-list {
+        //
+    }
 </style>

0e21c15ca70b3f87eacc9c809dfb6b8495a90b51
hedong <hedong@ibeesaas.com>
2019-10-15 20:07:53 +0800

    零件质检列表 供应商名称查询

--- src/pages/zjgl/material/index.vue
+++ src/pages/zjgl/material/index.vue
@@ -4,7 +4,6 @@
             <div class="flex-v-center search-bar" style="margin-right: 20px;">
                 <i class="icon f-20 c-8">search</i>
                 <input type="text" class="flex-item" v-model="filter.inspectNo" @change="$refs.list.update(true)" placeholder="送检单号" style="width: 200px;">
-                <i class="icon f-20 c-8">search</i>
                 <input
                     type="text"
                     class="flex-item"
@@ -12,6 +11,14 @@
                     @change="$refs.list.update(true)"
                     placeholder="采购合同号"
                     style="width: 200px;"
+                >
+                <input
+                        type="text"
+                        class="flex-item"
+                        v-model="filter.supplierName"
+                        @change="$refs.list.update(true)"
+                        placeholder="供应商名称"
+                        style="width: 200px;"
                 >
             </div>
         </div>
